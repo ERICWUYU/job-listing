80dacde697366dcdb3755da59f11c13eb1357269
吴昱 <wuyu1989610@gmail.com>
2017-04-10 21:25:27 +0800

    add admin panel and admin/jobs#index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409215007) do
+ActiveRecord::Schema.define(version: 20170410114419) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
